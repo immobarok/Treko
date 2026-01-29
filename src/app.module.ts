654d3f680b059f3c471b3c482be3d576e9eca5d0
immobarok <immobarokali2001@gmail.com>
2026-01-30 03:45:14 +0600

    minio and docker added

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,9 +14,10 @@ import { ResponseStandardizationInterceptor } from './common/interceptors/respon
 import { RequestLoggerMiddleware } from './common/middleware/request-logger.middleware';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { CommonModule } from './minio/common/common.module';
 
 @Module({
-  imports: [ContextModule, ConfigModule.forRoot(), AuthModule],
+  imports: [ContextModule, ConfigModule.forRoot(), AuthModule, CommonModule],
   controllers: [AppController],
   providers: [
     AppService,
